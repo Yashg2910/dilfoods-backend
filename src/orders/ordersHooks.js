1b8852104f65950fa5068cbea912f0b3dba86828
Yash Gupta <yash@cerah.co>
2023-04-09 04:13:44 +0530

    Allow only customers to create new order.

--- src/orders/ordersHooks.js
+++ src/orders/ordersHooks.js
@@ -1,3 +1,10 @@
+import jwtAuth from "../authentication/jwt.js";
+import {roleTypes} from "../users/roleTypes.js";
+
 export async function ordersHooks(req, res, next) {
+  if (req.method === "POST") {
+    const auth = jwtAuth.verify(req, res, [roleTypes.CUSTOMER]);
+    if (auth?.statusCode > 400) return;
+  }
   next()
 }
