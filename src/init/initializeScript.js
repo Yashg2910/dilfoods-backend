692018bcbff496f0ea2cc3a057bfa5252f184396
Yash Gupta <yash@cerah.co>
2023-04-09 04:04:42 +0530

    Corrected email

--- src/init/initializeScript.js
+++ src/init/initializeScript.js
@@ -3,19 +3,19 @@ import { UsersModel } from "../users/usersModel.js";
 export async function initializeDb() {
   const adminUser = {
     name: 'admin',
-    email: 'admin@dilFoods',
+    email: 'admin@dilfoods',
     password: 'admin123',
     role: 'ADMIN'
   };
   const staffUser = {
     name: 'staff',
-    email: 'staff@dilFoods',
+    email: 'staff@dilfoods',
     password: 'staff123',
     role: 'STAFF'
   };
   const customerUser = {
     name: 'customer',
-    email: 'customer@dilFoods',
+    email: 'customer@dilfoods',
     password: 'customer123',
     role: 'CUSTOMER'
   };
