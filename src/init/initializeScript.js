d7861503dd3f34f896f22606bd9a059665a798e3
Yash Gupta <yash@cerah.co>
2023-04-09 04:16:15 +0530

    Corrected creation of default users.

--- src/init/initializeScript.js
+++ src/init/initializeScript.js
@@ -1,28 +1,31 @@
 import { UsersModel } from "../users/usersModel.js";
+import bcrypt from "bcrypt";
 
 export async function initializeDb() {
   const adminUser = {
     name: 'admin',
-    email: 'admin@dilfoods',
+    email: 'admin@dilfoods.com',
     password: 'admin123',
     role: 'ADMIN'
   };
   const staffUser = {
     name: 'staff',
-    email: 'staff@dilfoods',
+    email: 'staff@dilfoods.com',
     password: 'staff123',
     role: 'STAFF'
   };
   const customerUser = {
     name: 'customer',
-    email: 'customer@dilfoods',
+    email: 'customer@dilfoods.com',
     password: 'customer123',
     role: 'CUSTOMER'
   };
   [adminUser, staffUser, customerUser].map(async (user) => {
     const existingUser = (await UsersModel.find({name: user.name}))[0];
     if (!existingUser) {
-      const modelUser = new UsersModel(user);
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(user.password, salt);
+      const modelUser = new UsersModel({...user, password: hashedPassword});
       const createdUser = await modelUser.save();
       console.log("Created user:", createdUser.name);
     }
